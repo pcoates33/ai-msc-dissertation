6d06bb0ef04bc4140c97b0ab5188bba0115d544d
pcoates <pcoates33@gmail.com>
2023-07-12 12:45:07 +0100

    Generates 3 random shapes

--- code/investigations/segmentation/shapes_with_templates.py
+++ code/investigations/segmentation/shapes_with_templates.py
@@ -15,13 +15,15 @@ class ShapeBuilder(mp.Process):
         super().__init__()
         self.count = count
         self.q = q
+        self.unique_id = 0
 
     def run(self):
         # Create the shapes and labels and add them to the q, then we just stop.
         shapes = [self.generate_shape(flip=False) for i in range(self.count)]
         self.q.put(shapes)
-        
-    def generate_shape(self, img_size_y=480, img_size_x=640, flip=True):
+
+    def new_geom(self, offset=None):
+        # Create a new geom for the mujoco xml
         # Generate disc, cube, or sphere
         # By default the image is flipped to give shape of '3, 480, 640'
         # Set flip=False to get an array of pixels instead. i.e. '480, 640, 3'
@@ -33,25 +35,21 @@ class ShapeBuilder(mp.Process):
 
         # Get x,y,z sizes between 0.01 and 0.02
         size_xyz = np.random.randint(10, 21, 3) / 1000
-        centre_z = "{0:.3f}".format(size_xyz[2])
 
-        size = centre_z
+        if shape_type == 'sphere':
+            size_xyz[0] = size_xyz[2]
+            size_xyz[1] = size_xyz[2]
+            shape_size = f"{size_xyz[2]:.3f}"
+            # for sphere x,y and z are equal
         if shape_type == 'box':
-            size = "{0:.3f} {1:.3f} {2:.3f}".format(size_xyz[0], size_xyz[1], size_xyz[2])
+            shape_size = f"{size_xyz[0]:.3f} {size_xyz[1]:.3f} {size_xyz[2]:.3f}"
         elif shape_type == 'disc':
             # a flattened cylinder
             shape_type = 'cylinder'
-            centre_z = size_xyz[2] / 20
-            size = "{0:.3f} {1}".format(2*size_xyz[0], centre_z)
-
-        # position the shape, x and y both between -0.025 and 0.025
-        pos_xy = np.random.randint(-25, 25, 2) / 1000
-        shape_pos = "{0:.3f} {1:.3f} {2}".format(pos_xy[0], pos_xy[1], centre_z)
-        shape_offset = f"{2.2*size_xyz[0]:.3f} {2.2*size_xyz[1]:.3f} 0.0"
-
-        # position the light, x and y both between -0.025 and 0.025
-        pos_xy = np.random.randint(-25, 25, 2) / 100
-        light_pos = "{0:.3f} {1:.3f} 0.3".format(pos_xy[0], pos_xy[1])
+            size_xyz[0] = 2*size_xyz[0]
+            size_xyz[1] = size_xyz[0]
+            size_xyz[2] = size_xyz[2] / 20
+            shape_size = f"{size_xyz[0]:.3f} {size_xyz[2]:.3f}"
 
         rgb = np.random.randint(2, 6, 3)/10
         # boost one of the rgb values to make it the predominant colour
@@ -65,6 +63,41 @@ class ShapeBuilder(mp.Process):
         # Rotate the shape about the z axis
         rotate = "0 0 {0}".format(np.random.randint(0, 90))
 
+        if offset is None:
+            offset = f"0.0 0.0 {size_xyz[2]:.3f}"
+        else:
+            # rho is 'radius' of object we're offsetting from. Add the 'radius' of the new geom to it.
+            rho, phi = offset
+            rho += np.hypot(size_xyz[0], size_xyz[1])
+            offset = f"{rho * np.cos(phi):.3f} {rho * np.sin(phi):.3f} {size_xyz[2]:.3f}"
+
+        label = {'colour': ['red','green','blue'][colour_main],
+                'colour_idx': colour_main,
+                'shape_type': s_type,
+                'shape_type_idx': shape_type_idx}
+        
+        self.unique_id += 1
+        asset = f"""<texture name="gt_{self.unique_id}" type="cube" builtin="gradient" mark="random" width="128" height="128" rgb1="{rgb_1}" rgb2="{rgb_2}" markrgb="{rgb_mark}" random="0.05"/>
+            <material name="gm_{self.unique_id}" texture="gt_{self.unique_id}" specular="0.0" texuniform="false"/>
+        """
+        body = f"""<geom name="obj{self.unique_id}" type="{shape_type}" size="{shape_size}" material="gm_{self.unique_id}" euler="{rotate}" pos="{offset}"/>
+        """
+
+        return asset, body, label, size_xyz
+            
+    def generate_shape(self, img_size_y=480, img_size_x=640, flip=True):
+        # Generate disc, cube, or sphere
+        # By default the image is flipped to give shape of '3, 480, 640'
+        # Set flip=False to get an array of pixels instead. i.e. '480, 640, 3'
+
+        # position the shape, x and y both between -0.025 and 0.025
+        pos_xy = np.random.randint(-25, 25, 2) / 1000
+        body_pos = f"{pos_xy[0]:.3f} {pos_xy[1]:.3f} 0.0"
+
+        # position the light, x and y both between -0.025 and 0.025
+        pos_xy = np.random.randint(-25, 25, 2) / 100
+        light_pos = "{0:.3f} {1:.3f} 0.3".format(pos_xy[0], pos_xy[1])
+        
         # camera pos, vary the z and y. z can go from 0.01 with y from -0.2 to -0.1, up to 
         # 0.2 with y from -0.2 to 0.0
         cam_z = np.random.randint(1, 41)
@@ -73,38 +106,52 @@ class ShapeBuilder(mp.Process):
         cam_z = cam_z / 100
         cam_pos = "0 {0:.2f} {1:.2f}".format(cam_y, cam_z)
 
-        shape_xml = """
+        # drop the first one in the middle
+        asset_1, body_1, label_1, size_xyz_1 = self.new_geom()
+
+        # offset second shape so that it doesn't overlap the first.
+        print(size_xyz_1)
+        offset_angle = np.random.randint(0, 360)
+
+        rho = np.hypot(size_xyz_1[0], size_xyz_1[1])
+        nudge = 1.0 + (np.random.randint(0, 10)/10)
+        asset_2, body_2, label_2, size_xyz_2 = self.new_geom([rho * nudge, np.radians(offset_angle)])
+
+        offset_angle = offset_angle + np.random.randint(30, 331)
+        nudge = 1.0 + (np.random.randint(0, 10)/10)
+        asset_3, body_3, label_3, size_xyz_3 = self.new_geom([rho * nudge, np.radians(offset_angle)])
+
+        shape_xml = f"""
         <mujoco model="shape">
 
         <asset>
             <texture name="grid" type="2d" builtin="checker" rgb1=".95 .95 .95" rgb2=".9 .9 .9" width="50" height="50"/>
             <material name="grid" texture="grid" texrepeat="128 128"/>
-            <texture name="geom_texture_1" type="cube" builtin="gradient" mark="random" width="128" height="128" rgb1="{3}" rgb2="{4}" markrgb="{5}" random="0.05"/>
-            <material name="geom_material_1" texture="geom_texture_1" specular="0.0" texuniform="false"/>
-            <texture name="geom_texture_2" type="cube" builtin="gradient" mark="random" width="128" height="128" rgb1="{3}" rgb2="{4}" markrgb="{5}" random="0.05"/>
-            <material name="geom_material_2" texture="geom_texture_2" specular="0.0" texuniform="false"/>
+            {asset_1}
+            {asset_2}
+            {asset_3}
         </asset>
 
         <worldbody>
             <geom size=".6 .6 .01" type="plane" material="grid"/>
-            <light diffuse=".5 .5 .5" pos="{7}" mode="targetbody" target="camera_target"/>
-            <camera name="closeup" pos="{8}" mode="targetbody" target="camera_target"/>
+            <light diffuse=".5 .5 .5" pos="{light_pos}" mode="targetbody" target="camera_target"/>
+            <camera name="closeup" pos="{cam_pos}" mode="targetbody" target="camera_target"/>
             <body name="camera_target" pos="0 0 0"/>
-            <body name="shape" pos="{2}">
-                <geom name="one" type="{0}" size="{1}" material="geom_material_1" euler="{6}" pos="0.0 0.0 0.0"/>
-                <geom name="two" type="{0}" size="{1}" material="geom_material_2" euler="{6}" pos="{9}"/>
+            <body name="shape" pos="{body_pos}">
+                {body_1}
+                {body_2}
+                {body_3}
             </body>
         </worldbody>
 
         </mujoco>
-        """.format(shape_type, size, shape_pos, rgb_1, rgb_2, rgb_mark, rotate, light_pos, cam_pos, shape_offset)
+        """
 
         # print(shape_xml)
         # print('{0} {1}'.format(['red','green','blue'][colour_main], s_type))
-        label = {'colour': ['red','green','blue'][colour_main],
-                'colour_idx': colour_main,
-                'shape_type': s_type,
-                'shape_type_idx': shape_type_idx}
+        # TODO : work out how to set up the label with sensible data
+        label = label_1
+
 
         shape_model = mujoco.MjModel.from_xml_string(shape_xml)
         renderer = mujoco.Renderer(shape_model, img_size_y, img_size_x)
